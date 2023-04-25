486fd871bc2c9fa2bd0b7527ccb16b7b6deb00b1
Darold K. Trench <darold.trench@gmail.com>
2023-04-25 18:55:57 -0400

    Delete trabajos directory
    drill off

--- trabajos/drill/assetes/js/logic.js
+++
@@ -1,9 +0,0 @@
-
-$('#buttonBg').click(function(){
-    if($('#check').prop('checked') === true){
-        console.log('primer')
-        $('#code').toggleClass('d-none', 'd-block')
-    }else{
-        alert('Para obtener un codigo de descuento debe aceptar los términos y condiciones')
-    }
-})
